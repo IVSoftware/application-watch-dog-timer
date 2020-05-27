c234ebb909ff1d9b9d8610780ec872b814602c6a
IVSoftware <tgregor@ivsoftware.com>
2020-05-27 01:52:16 -0600

    Working prelim example.

--- application-watch-dog-timer/Form1.cs
+++ application-watch-dog-timer/Form1.cs
@@ -6,30 +6,50 @@ namespace application_watch_dog_timer
 {
     enum TimeOutState
     {
-        WakeUp,
-        Sleeping
+        Exit,
+        Warning,
+        Sleeping,
+        WakeUp
     }
-    public partial class Form1 : Form
+    public partial class Form1 : Form, IMessageFilter
     {
         public Form1()
         {
             InitializeComponent();
-            _wdt = new Timer();
-            
-            _wdt.Interval = 10000; // Use a very short time-out for this demo
+            _wdt = new Timer();            
+            _wdt.Interval = 5000; // Use a very short time-out for this demo
             _wdt.Tick += _wdt_Tick;
+        }
+
+        const int WM_MOUSEMOVE = 0x0200; // WinOS Message
+        public bool PreFilterMessage(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_MOUSEMOVE:
+                    TimeOutState = TimeOutState.WakeUp;
+                    break;
+            }
+            return false; // Do not suppress downstream message
         }
 
         protected override void OnHandleCreated(EventArgs e)
         {
             base.OnHandleCreated(e);
+            Application.AddMessageFilter(this);
+            // Wait for Form1 window handle to exist before starting WDT
+            TimeOutState = TimeOutState.WakeUp;
         }
 
         private void _wdt_Tick(object sender, System.EventArgs e)
-        { 
+        {
+            // A tick reduces the TimeOutState by 1
+            TimeOutState = (TimeOutState)(TimeOutState - 1);
         }
         
         Timer _wdt; // Watch-Dog Timer
+
+        // Time out state machine
         TimeOutState TimeOutState
         {
             get => _timeOutState;
@@ -38,26 +58,31 @@ namespace application_watch_dog_timer
                 if(value != _timeOutState)  // Check if property has changed
                 {
                     _timeOutState = value;
+                    switch (TimeOutState)
+                    {
+                        case TimeOutState.WakeUp:
+                            _wdt.Start();
+                            break;
+                        case TimeOutState.Exit:
+                            _wdt.Stop();
+                            Application.Exit();
+                            return;
+                    }
                     // In a timer callback that changes the UI, it's
                     // best to post the action in the message queue.
                     BeginInvoke((MethodInvoker)delegate
                     {
                         textBox1.AppendText(_timeOutState.ToString() + Environment.NewLine);
+                        if(TimeOutState == TimeOutState.Warning)
+                        {
+                            textBox1.AppendText(
+                                "Closing in " + (_wdt.Interval/1000).ToString() + " seconds." + Environment.NewLine);
+                        }
+                        textBox1.Select(textBox1.TextLength, 0);
                     });
                 }
             }
         }
-
         TimeOutState _timeOutState = (TimeOutState)(-1);    // Initialize to invalid state
-
-        // Just override this method. No need to subscribe with += to a MouseMove event.
-        protected override void OnMouseMove(MouseEventArgs e)
-        {
-            base.OnMouseMove(e);
-            if(!_wdt.Enabled)
-            {
-
-            }
-        }
     }
 }
